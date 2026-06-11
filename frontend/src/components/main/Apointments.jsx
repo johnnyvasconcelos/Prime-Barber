d944fdd9c660bccee94f80f06f01f40f1e353d7f
Jhonatan <jvasconcelosbittencourt@gmail.com>
2026-06-10 21:43:23 -0300

    banco de dados tabela principal, finalizando home

--- frontend/src/components/main/Apointments.jsx
+++ frontend/src/components/main/Apointments.jsx
@@ -1,5 +1,51 @@
 import { FaDollarSign } from "react-icons/fa";
+import { useState, useEffect } from "react";
 const Apointments = () => {
+  const [dados, setDados] = useState([]);
+  useEffect(() => {
+    fetch("http://192.168.1.4:3500/")
+      .then((resposta) => resposta.json())
+      .then((dados) => {
+        setDados(dados);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  const dadosTabela = [];
+  let limite = 0;
+
+  if (dados.length > 0) {
+    const ultimaData = dados.reduce((recente, atual) => {
+      return atual.data > recente.data ? atual : recente;
+    }, dados[0]).data;
+
+    for (const dado of dados) {
+      if (limite > 5) {
+        break;
+      }
+
+      if (dado.data === ultimaData) {
+        const semSegs = dado.horario ? dado.horario.slice(0, 5) : "";
+
+        dadosTabela.push(
+          <tr key={dado.id}>
+            <td>{semSegs}</td>
+            <td>{dado.cliente}</td>
+            <td>{dado.nome_servico}</td>
+            <td>{dado.profissional}</td>
+            <td>{dado.faturamento}</td>
+            {dado.atendido == 1 ? (
+              <td className="confirmed">Confirmado</td>
+            ) : (
+              <td className="pending">Pendente</td>
+            )}
+          </tr>,
+        );
+
+        limite += 1;
+      }
+    }
+  }
   return (
     <section className="content__apointments">
       <div className="content__container">
@@ -21,46 +67,7 @@ const Apointments = () => {
               </thead>
               <tbody>
                 {/* dados de clientes */}
-                <tr>
-                  <td>09:00</td>
-                  <td>João Silva</td>
-                  <td>Corte + Barba</td>
-                  <td>Ricardo</td>
-                  <td>R$ 39,00</td>
-                  <td className="confirmed">Confirmado</td>
-                </tr>
-                <tr>
-                  <td>09:00</td>
-                  <td>João Silva</td>
-                  <td>Corte + Barba</td>
-                  <td>Ricardo</td>
-                  <td>R$ 39,00</td>
-                  <td className="confirmed">Confirmado</td>
-                </tr>
-                <tr>
-                  <td>09:00</td>
-                  <td>João Silva</td>
-                  <td>Corte Simples</td>
-                  <td>Ricardo</td>
-                  <td>R$ 39,00</td>
-                  <td className="confirmed">Confirmado</td>
-                </tr>
-                <tr>
-                  <td>09:00</td>
-                  <td>João Silva</td>
-                  <td>Corte + Barba</td>
-                  <td>Joel</td>
-                  <td>R$ 39,00</td>
-                  <td className="pending">Pendente</td>
-                </tr>
-                <tr>
-                  <td>09:00</td>
-                  <td>João Silva</td>
-                  <td>Corte + Barba</td>
-                  <td>Ricardo</td>
-                  <td>R$ 39,00</td>
-                  <td className="pending">Pendente</td>
-                </tr>
+                {dadosTabela}
               </tbody>
             </table>
           </div>
@@ -73,19 +80,126 @@ const Apointments = () => {
             <tbody>
               <tr>
                 <td>Faturamento hoje</td>
-                <td className="today">R$ 400,00</td>
+                <td className="today">
+                  {" "}
+                  {dados.length > 0
+                    ? (() => {
+                        const ultimaData = dados.reduce((recente, atual) => {
+                          return new Date(atual.data) > new Date(recente.data)
+                            ? atual
+                            : recente;
+                        }, dados[0])?.data;
+                        let sum = 0;
+                        for (let dadoX of dados) {
+                          if (dadoX.data === ultimaData) {
+                            sum += Number(dadoX.faturamento);
+                          }
+                        }
+                        return `R$ ${sum.toFixed(2).replace(".", ",")}`;
+                      })()
+                    : "R$ 0,00"}
+                </td>
               </tr>
               <tr>
                 <td>Faturamento esta semana</td>
-                <td>R$ 400,00</td>
+                <td>
+                  {dados.length > 0
+                    ? (() => {
+                        const ultimaDataStr = dados.reduce((recente, atual) => {
+                          return new Date(atual.data) > new Date(recente.data)
+                            ? atual
+                            : recente;
+                        }, dados[0])?.data;
+
+                        const dataMaxima = new Date(ultimaDataStr);
+
+                        const dataLimite = new Date(ultimaDataStr);
+                        dataLimite.setDate(dataLimite.getDate() - 7);
+
+                        let sum = 0;
+                        for (let dadoX of dados) {
+                          const dataAtual = new Date(dadoX.data);
+
+                          if (
+                            dataAtual > dataLimite &&
+                            dataAtual <= dataMaxima
+                          ) {
+                            sum += Number(dadoX.faturamento);
+                          }
+                        }
+
+                        return `R$ ${sum.toFixed(2).replace(".", ",")}`;
+                      })()
+                    : "R$ 0,00"}
+                </td>
               </tr>
               <tr>
                 <td>Faturamento este mês</td>
-                <td>R$ 400,00</td>
+                <td>
+                  {dados.length > 0
+                    ? (() => {
+                        const ultimaData = dados.reduce((recente, atual) => {
+                          return new Date(atual.data) > new Date(recente.data)
+                            ? atual
+                            : recente;
+                        }, dados[0])?.data;
+
+                        const data = new Date(ultimaData);
+                        const ano = data.getFullYear();
+                        const mes = data.getMonth();
+
+                        let sum = 0;
+                        for (let dadoX of dados) {
+                          const dataAtual = new Date(dadoX.data);
+                          if (
+                            dataAtual.getFullYear() === ano &&
+                            dataAtual.getMonth() === mes
+                          ) {
+                            sum += Number(dadoX.faturamento);
+                          }
+                        }
+
+                        return `R$ ${sum.toFixed(2).replace(".", ",")}`;
+                      })()
+                    : "R$ 0,00"}
+                </td>
               </tr>
               <tr>
                 <td>Ticket médio</td>
-                <td>R$ 400,00</td>
+                <td>
+                  {dados.length > 0
+                    ? (() => {
+                        const ultimaDataStr = dados.reduce((recente, atual) => {
+                          return new Date(atual.data) > new Date(recente.data)
+                            ? atual
+                            : recente;
+                        }, dados[0])?.data;
+
+                        const dataLimite = new Date(ultimaDataStr);
+                        dataLimite.setDate(dataLimite.getDate() - 7);
+
+                        let faturamentoTotal = 0;
+                        let totalAtendimentos = 0;
+
+                        for (let dadoX of dados) {
+                          const dataAtual = new Date(dadoX.data);
+
+                          if (
+                            dataAtual > dataLimite &&
+                            dataAtual <= new Date(ultimaDataStr)
+                          ) {
+                            faturamentoTotal += Number(dadoX.faturamento);
+                            totalAtendimentos++;
+                          }
+                        }
+
+                        const ticketMedio =
+                          faturamentoTotal / totalAtendimentos;
+
+                        return `R$ ${ticketMedio.toFixed(2).replace(".", ",")}`;
+                      })()
+                    : "R$ 0,00"}
+                </td>
               </tr>
             </tbody>
           </table>
