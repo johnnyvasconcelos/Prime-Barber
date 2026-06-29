6fc72ab846adb96f9fb5d503f98bf1396fd642ac
Jhonatan <jvasconcelosbittencourt@gmail.com>
2026-06-29 16:29:36 -0300

    ajustes

--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT,
+  host: process.env.MYSQL_HOST || process.env.DB_HOST,
+  user: process.env.MYSQL_USER || process.env.DB_USER,
+  password: process.env.MYSQL_PASSWORD || process.env.DB_PASSWORD,
+  database: process.env.MYSQL_DATABASE || process.env.DB_NAME,
+  port: process.env.MYSQL_PORT || process.env.DB_PORT || 12207,
   ssl: {
     rejectUnauthorized: false,
   },
